5ebe5d4f4a79ab6cd7e54cf92d13369eb110dba2
Sourav Singh <sourav.singh@winfosolutions.com>
2022-12-08 12:19:49 +0530

    removing bcrypt

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var jwt = require("jsonwebtoken");
 const path = require("path");
 const hbs = require("hbs");
 require("./src/db/conn");
-const bcrypt = require("bcrypt");
+// const bcrypt = require("bcrypt");
 const port = process.env.PORT || 3000;
 const Register = require("./src/models/schema");
 
@@ -28,71 +28,71 @@ app.get("/login", (req, res) => {
   res.render("login");
 });
 
-app.post("/login", async (req, res) => {
-  try {
-    const name = req.body.name;
-    const password = req.body.psw;
-    const result = await Register.findOne({ email: name });
-    const match = await bcrypt.compare(password, result.password);
-    if (match) {
-      console.log("password matched");
-      const token = await createEmployee.createToken(savingData);
-      console.log("token is generated ", token);
-      res.status(200).render("index");
-    } else {
-      res.status(400).send("email & password are not matching");
-    }
-    // if (result.password === password) {
-    //   res.status(200).render("index");
-    // } else {
-    //   res.status(400).send("email & password are not matching");
-    // }
-  } catch (err) {
-    res.status(400).send(err);
-  }
-});
+// app.post("/login", async (req, res) => {
+//   try {
+//     const name = req.body.name;
+//     const password = req.body.psw;
+//     const result = await Register.findOne({ email: name });
+//     const match = await bcrypt.compare(password, result.password);
+//     if (match) {
+//       console.log("password matched");
+//       const token = await createEmployee.createToken(savingData);
+//       console.log("token is generated ", token);
+//       res.status(200).render("index");
+//     } else {
+//       res.status(400).send("email & password are not matching");
+//     }
+//     // if (result.password === password) {
+//     //   res.status(200).render("index");
+//     // } else {
+//     //   res.status(400).send("email & password are not matching");
+//     // }
+//   } catch (err) {
+//     res.status(400).send(err);
+//   }
+// });
 
 app.get("/register", (req, res) => {
   console.log(req.params);
   res.render("register");
 });
 
-app.post("/register", async (req, res) => {
-  try {
-    if (req.body.psw === req.body.pswRepeat) {
-      const salt = await bcrypt.genSalt(10);
-      const pass = await bcrypt.hash(req.body.psw, salt);
+// app.post("/register", async (req, res) => {
+//   try {
+//     if (req.body.psw === req.body.pswRepeat) {
+//       const salt = await bcrypt.genSalt(10);
+//       const pass = await bcrypt.hash(req.body.psw, salt);
 
-      const createEmployee = new Register({
-        email: req.body.email,
-        password: pass,
-        something: "sfljs",
-      });
+//       const createEmployee = new Register({
+//         email: req.body.email,
+//         password: pass,
+//         something: "sfljs",
+//       });
 
-      console.log("registration part is success");
+//       console.log("registration part is success");
 
-      const token = await createEmployee.createToken(savingData);
+//       const token = await createEmployee.createToken(savingData);
 
-      // using callback function here.
+//       // using callback function here.
 
-      res.status(201).render("login");
-    } else {
-      res.status(404).send("password is not matchingh");
-    }
-  } catch (err) {
-    res.status(400).send(err);
-  }
-});
+//       res.status(201).render("login");
+//     } else {
+//       res.status(404).send("password is not matchingh");
+//     }
+//   } catch (err) {
+//     res.status(400).send(err);
+//   }
+// });
 
 app.listen(port, () => {
   console.log(`server is listening on ${port}`);
 });
 
-const securePassword = async (pass) => {
-  const salt = await bcrypt.genSalt(10);
-  const pwd = await bcrypt.hash(pass, salt);
-  console.log(pwd);
-};
+// const securePassword = async (pass) => {
+//   const salt = await bcrypt.genSalt(10);
+//   const pwd = await bcrypt.hash(pass, salt);
+//   console.log(pwd);
+// };
 
 // securePassword("sourav");
 
